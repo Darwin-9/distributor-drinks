fdf4bb7dce916d8a597b7f48c3272ee2c7efea1b
Darwin-9 <darwincuellarmorales.2006@gmail.com>
2025-07-03 06:13:23 -0500

    jwtc

--- crud_basic/src/main/java/com/sena/crud_basic/CrudBasicApplication.java
+++ crud_basic/src/main/java/com/sena/crud_basic/CrudBasicApplication.java
@@ -8,6 +8,11 @@ public class CrudBasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudBasicApplication.class, args);
+		
+		// // Generar una nueva clave HMAC-SHA256 segura:
+        //Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        //byte[] keyBytes = key.getEncoded();
+        //System.out.println("Generated key: " + Base64.getEncoder().encodeToString(keyBytes));
 	}
 
 }
